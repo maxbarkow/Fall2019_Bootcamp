86bcbe719e06ebd15dd5fcb54624f2a07d7e5ea5
maxbarkow <maxbarkow@ufl.edu>
2019-09-17 16:44:29 -0400

    updated file structure

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,28 +1,52 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
-  Make sure to save your DB's uri in the config file, then import it with a require statement!
- */
+/*
+Import modules/files you may need to correctly run the script.
+Make sure to save your DB's uri in the config file, then import it with a require statement!
+*/
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+mongoose = require('mongoose'),
+Schema = mongoose.Schema,
+Listing = require('./ListingSchema.js'),
+config = require('./config'),
+listingData;
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+/*
+//get fs.readFile from bootcamp1 inside readfile get loop of listing file variable, make a new mongoos listing model
+and save to database
+Instantiate a mongoose model for each listing object in the JSON file,
+and then save it to your Mongo database
+//see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
-  //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
+*/
+fs.readFile('listings.json', 'utf8', function(err, data) {
 
-  Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
- */
+  if (err) { throw (err)
+  }
+  listingData = JSON.parse(data);
+  listingData.entries.forEach(function(entry){
+    var savedEntry = Listing(entry);
+
+    savedEntry.save(function(err){
+      if(err){
+        console.log("Error: Did not save to DB")
+        throw err;
+      }
+      console.log("Saved to database")
+    });
 
 
-/*  
-  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+  });
+})
+
+
+/*
+Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that
+it saved everything correctly.
+*/
--- queries.js
+++ queries.js
@@ -1,3 +1,4 @@
+'use strict';
 /* Add all the required libraries*/
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
@@ -5,29 +6,57 @@
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  Listing = require('./ListingSchema.js'),
+  config = require('./config'),
+  listing;
+
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+
 var findLibraryWest = function() {
-  /* 
-    Find the document that contains data corresponding to Library West,
-    then log it to the console. 
-   */
+  Listing.find({name: 'Library West'}, function (err,entry){
+if (err) throw err;
+  console.log(entry);
+
+ });
 };
+
 var removeCable = function() {
+  Listing.findOneAndDelete({code: 'CABL'}, function (err,entry){
+    if (err) throw err;
+    console.log("Deleted CABL")
+  });
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
 };
 var updatePhelpsMemorial = function() {
+  Listing.findOneAndUpdate({name:'Phelps Laboratory'}, {address: '1953 Museum Road, Gainesville, FL 32603, United States', latitude: 29.644890, longitude: -82.348834}, {new: true}, function(err, entry){
+if (err) throw err;
+console.log(entry)
+});
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find({}, function(err, entry){
+     if (err) throw err;
+    // console.log(entry)
+    console.log("All listings")
+     console.log(JSON.stringify(entry, null, 1));
+   });
 };
 
 findLibraryWest();
